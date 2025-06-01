98fe2853065b228b61be6b3ad2a3e6e047c746da
Benedict-Ik <benedict.ik98@gmail.com>
2025-06-01 14:48:13 +0100

    Created AppDbContext Class and ran migrations

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 - Here, we created a `Data` folder that will house our `AppDbContext` class, and contains a `DbSet<User>` property to represent the users in our database.
 
+- Registered the `AppDbContext` in the `Program.cs` file using the `AddDbContext` method, which allows us to inject the database context into our controllers.
+```C#
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+```
 
 - Next, we added the below commands to run the migrations and update the database via athe package manager console:
 ```bash
--- obj/Debug/net8.0/CustomBasicAuth.AssemblyInfo.cs
+++ obj/Debug/net8.0/CustomBasicAuth.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomBasicAuth")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5974af1cf9f6431add25a0ccd5d145c235a005e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f1cb6ceca3e5ffabbc629900898cbb2f3c03619")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomBasicAuth")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomBasicAuth")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
